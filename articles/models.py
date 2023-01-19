9299da98fe89a3b46862cf38e39ec11d161eb561
Robb McMahan <robb@etxtech.net>
2023-01-19 17:24:08 -0600

    additions and testing version

--- articles/models.py
+++ articles/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=120)
     content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    pubish = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
