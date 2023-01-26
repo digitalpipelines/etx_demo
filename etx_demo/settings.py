ce712f0428281cf578471b2998c137bac8aa19c2
Robb McMahan <robb@etxtech.net>
2023-01-25 18:50:46 -0600

    testing

--- etx_demo/settings.py
+++ etx_demo/settings.py
@@ -27,11 +27,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = 1 #str(os.environ.get('DEBUG')) == "1" # 1 is True
 
 ENV_ALLOWED_HOST = '*'#os.environ.get('DJANGO_ALLOWED_HOST') or None
-ALLOWED_HOSTS = []
-if not DEBUG:
-    ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOST')]
-print('Allowed hosts ' + str(ALLOWED_HOSTS))    
-print('env Allowed hosts ' + str(ENV_ALLOWED_HOST))   
+ALLOWED_HOSTS = ['stingray-app-vrxnw.ondigitalocean.app']
+# if not DEBUG:
+#     ALLOWED_HOSTS += [os.environ.get('DJANGO_ALLOWED_HOST')]
+# print('Allowed hosts ' + str(ALLOWED_HOSTS))    
+# print('env Allowed hosts ' + str(ENV_ALLOWED_HOST))   
 
 # Application definition
 
